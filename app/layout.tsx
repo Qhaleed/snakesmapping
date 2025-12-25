a59a8e4fa09195597573c61a2fe879854c4387cd
Qhaleed <reslykadiri@gmail.com>
2025-12-26 00:07:55 +0800

    feat: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,14 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+  variable: "--font-geist-mono",  
   subsets: ["latin"],
 });
 
